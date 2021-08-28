1d9a970de0e8bdf8d6dfbf9904410addd8940fe5
Anksharskarp <aerialconnection98@gmail.com>
2021-08-28 13:51:37 -0400

    Removed extraneous notes files

--- Week1/BadCode.java
+++
@@ -1,7 +0,0 @@
-public class BadCode {
-    public static void main(String[] args) {
-        //Types of variables involved in datatype
-        //operations must always match their definitions.
-        //String s = "123" * 2; //This will be caught by the compiler.
-    }
-}
